453d555aa2357295e4531ae0496f989a0f6e630d
rondon1947 <rohan.nishant1999@gmail.com>
2020-05-06 19:29:57 +0530

    Add Sign Up and Sign In Feature

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var app = express();
 
 mongoose.connect("mongodb://localhost:27017/secret", { useUnifiedTopology: true, useNewUrlParser: true });
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
    secret: "Bruce Wayne is the Best",
    resave: false,
@@ -16,6 +17,7 @@ app.use(require("express-session")({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+passport.use(new passportLocal(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -29,6 +31,28 @@ app.get("/secret", function (req, res){
 
 app.get("/register", function (req, res) {
    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+   User.register(new User({username: req.body.username}), req.body.password, function (err, user) {
+      if (err){
+         console.log(err);
+         return res.render("register");
+      }
+      passport.authenticate("local")(req, res, function (){
+         res.redirect("/secret");
+      });
+   });
+});
+
+app.get("/login", function (req, res){
+   res.render("login");
+});
+
+app.post("/login",passport.authenticate("local", {
+   successRedirect: "/secret",
+   failureRedirect: "/login"
+}), function (req, res){
 });
 
 app.listen(3000, function(){
