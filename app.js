f3ff8cb4d2559699d4e6cb5094be891d6bba4771
rondon1947 <rohan.nishant1999@gmail.com>
2020-05-06 15:59:10 +0530

    Add secret page

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var passport = require("passport");
+var passportLocal = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
+var User = require("./models/user");
 var app = express();
 
+mongoose.connect("mongodb://localhost:27017/secret", { useUnifiedTopology: true, useNewUrlParser: true });
 app.set("view engine", "ejs");
+app.use(require("express-session")({
+   secret: "Bruce Wayne is the Best",
+   resave: false,
+   saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
    res.render("home");
@@ -9,6 +25,10 @@ app.get("/", function(req, res){
 
 app.get("/secret", function (req, res){
    res.render("secret");
+});
+
+app.get("/register", function (req, res) {
+   res.render("register");
 });
 
 app.listen(3000, function(){
